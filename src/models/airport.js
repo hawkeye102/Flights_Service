e5c4406a3f1163b25382c83d7cbb07cdf6b6e3bc
hawkeye102 <outofbox973@gmail.com>
2025-02-04 23:18:53 +0530

    added the flights model with foreign key constraint  during model defination '''  npx sequelize model:generate --name Flight --attributes flightNumber:string,  airplaneId:integer,departureAirportId:integer,arrivalAirportId:integer,arrivalTime:date,  departureTime:date,price:integer,boardingGate:string,totalSeats:integer;
    '''

--- src/models/airport.js
+++ src/models/airport.js
@@ -16,6 +16,14 @@ module.exports = (sequelize, DataTypes) => {
         onUpdate:'CASCADE',
         onDelete:'CASCADE'
       })
+      this.hasMany(models.Flight,{
+        foreignKey:'departureAirportId',
+        onDelete:'CASCADE'
+      })
+      this.hasMany(models.Flight,{
+        foreignKey:'arrivalAirportId',
+        onDelete:'CASCADE'
+      })
     }
   }
   Airport.init({
